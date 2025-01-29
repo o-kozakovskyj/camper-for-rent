cb56da1275762d74f6548a1fea26c7df309f14f5
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2025-01-29 19:22:18 +0200

    add transletes

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,10 +2,11 @@ import Header from "../components/Header/Header";
 import { Outlet } from "react-router-dom";
 import * as Styled from "./Layout.styled";
 
-const Layout = () => {
+const Layout = ({locale, setLocale}) => {
+  
   return (
     <Styled.LayoutContainer>
-      <Header />
+      <Header locale={locale} setLocale={setLocale} />
       <Outlet />
     </Styled.LayoutContainer>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import * as Styled from "./Home.styled.jsx"
 import { ButtonGeneral } from "../../components/ButtonGeneral/ButtonGeneral.jsx";
+import { useIntl } from "react-intl";
 
 const HomePage = () => {
   const navigate = useNavigate();
   const handleStart = () => navigate("/catalog");
-
+  const intl = useIntl();
   return (
     <Styled.HomeContainer>
       <Styled.WrapperImage>
         <Styled.WrapperText>
-          <Styled.HomeTitle>Campers of your dreams</Styled.HomeTitle>
-          <Styled.HomeText>You can find everything you want in our catalog</Styled.HomeText>
+          <Styled.HomeTitle>
+            {intl.formatMessage({ id: "homeTitle" })}
+          </Styled.HomeTitle>
+          <Styled.HomeText>
+            You can find everything you want in our catalog
+          </Styled.HomeText>
           <ButtonGeneral onClick={handleStart}>View Now</ButtonGeneral>
         </Styled.WrapperText>
       </Styled.WrapperImage>
